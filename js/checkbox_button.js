17c0559d4faee359f6157ff52be66d66c944a041
RogerJohanssonITHS <89128014+RogerJohanssonITHS@users.noreply.github.com>
2022-02-23 20:54:57 +0100

    courses.js in working order

--- js/checkbox_button.js
+++ js/checkbox_button.js
@@ -1,15 +1,6 @@
-const btn_get = document.querySelector('#btn_get');
-//const btn_check = document.querySelector('#btn_check');
-btn_get.addEventListener('click', (event) => {
-    let checkboxes = document.querySelectorAll('input[name="learningpath"]:checked');
-    let values = [];
-    checkboxes.forEach((checkbox) => {
-        values.push(checkbox.value);
-    });
-    alert(values);
-});
 
-const btn_check = document.querySelector('#btn_clear');
+
+/* const btn_check = document.querySelector('#btn_clear');
 btn_clear.onclick = uncheckAll;
 
 function uncheckAll(checked = false) {
@@ -19,7 +10,7 @@ function uncheckAll(checked = false) {
     });
     var myList = document.getElementById("myFilteredCourseList");
     myList.remove();
-}
+} */
 
 /* function checkAll() {
     check();
